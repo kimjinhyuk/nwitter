ecea4b700aacb16331472e98d066f0edf1bd834d
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-06 06:00:09 +0900

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+
+
+// console.log(fbase)
 
 ReactDOM.render(
   <React.StrictMode>
