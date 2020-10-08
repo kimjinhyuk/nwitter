e8450149f0ec1e84ed02f4bc26b7d58130781e96
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-08 12:29:21 +0900

    update login - logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom"
+import React from "react";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom"
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigaion from "components/Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigaion />}
             <Switch>
                 {isLoggedIn ? (
                     <>
                         <Route exact path="/">
                             <Home/>
                         </Route>
+                        <Route exact path="/profile">
+                            <Profile/>
+                        </Route>
+                        {/* <Redirect from="*" to="/" /> */}
                     </>
                 ) : (
+                    <>
                     <Route exact path="/">
                         <Auth />
                     </Route>
+                    {/* <Redirect from="*" to="/" /> */}
+                    </>
                 )}
             </Switch>
         </Router>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,11 +1,14 @@
 import React , {useState} from "react";
+import { authService, firebaseInstance } from "fbase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
       const {
-        target : { name, value},
+        target : { name, value },
       } = event;
       if(name === "email"){
         setEmail(value);
@@ -13,19 +16,50 @@ const Auth = () => {
         setPassword(value);
       }
     };
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
       event.preventDefault();
+      try{
+        let data;
+        if(newAccount){
+          // create account
+          data = await authService.createUserWithEmailAndPassword(email, password);
+        } else {
+          //  Log in
+          data = await authService.signInWithEmailAndPassword(email,password);
+        }
+        console.log(data);
+      } catch(error){
+        setError(error.message);
+      }
     };
+  const toggleAccount = () => setNewAccount(prev => !prev);
+  const onSocialClick = async(event) => {
+    const {
+      target : {name},
+    } = event;
+    let provider;
+    if(name === "google"){
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }else if(name === "github"){
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
 				<input name="email" type="email" placeholder="Email" required="required" value={email} onChange={onChange} />
 				<input name="password" type="password" placeholder="Password" required="required" value={password} onChange={onChange} />
-				<input type="submit" value="Log In"/>
+				<input type="submit" value={newAccount ? "Create Account" : "Log In" } />
+        {error}
 			</form>
+  <span onClick={toggleAccount}>
+    {newAccount ? "Sign in" : "Create Account"}
+  </span>
 			<div>
-				<button>Continue with Google</button>
-				<button>Continue with Github</button>
+				<button onClick={onSocialClick} name="google">Continue with Google</button>
+				<button onClick={onSocialClick} name="github">Continue with Github</button>
 			</div>
 		</div>
 	);
