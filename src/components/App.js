897be88bd74dde7c60589ddca13e360180ab9389
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-11 03:25:05 +0900

    complete img file upload

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,6 @@ function App() {
   return (
     <>
       {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Checking Log in..."}
-      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
 }
