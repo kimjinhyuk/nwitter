2da3cc76cf40a4513a46d15e7ed1be76dfa7e0b4
kimjinhyuk <kimjinhyuk2@gmail.com>
2020-10-12 00:52:48 +0900

    Updates

---
+++ precache-manifest.6562e2a72adf252466dbc909243f4ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba213499ba7f594dab207d5d783449f",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "9c0220c60a34cf07ed17",
+    "url": "/nwitter/static/css/main.86fde8f1.chunk.css"
+  },
+  {
+    "revision": "dec0a363d7615260c09c",
+    "url": "/nwitter/static/js/2.4203db7b.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/nwitter/static/js/2.4203db7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0220c60a34cf07ed17",
+    "url": "/nwitter/static/js/main.7c386e72.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
